d224e4b0340b66125840f4159a526b6abac77d39
Kameron Peart <peartk1027@students.bowiestate.edu>
2026-09-17 10:46:27 -0400

    3rd push -Inheritance

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-     //created two person object
+        //created two person object
         Person p1 = new Person();
 
 //        Person p2 = new Person(72, 25, "Black", "Barack Obama", 170);
@@ -10,6 +10,11 @@ public class Main {
         Person p2 = new Person(72, 70);
         System.out.println(p2.age);
 
-        Person p3= p2.Call_Private();
+        Person p3 = p2.Call_Private();
+
+        Student s1 = new Student();
+        s1.age = 20;
+        s1.gpa = 4.00;
+        System.out.println(s1.gpa);
     }
 }
