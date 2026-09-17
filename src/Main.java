6b6e99ac6b7cead970e15d996dd419d69321cc31
Kameron Peart <peartk1027@students.bowiestate.edu>
2026-09-17 10:21:57 -0400

    2nd push -Inheritance

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
      //created two person object
         Person p1 = new Person();
 
-        Person p2 = new Person(72, 25, "Black", "Barack Obama", 170);
+//        Person p2 = new Person(72, 25, "Black", "Barack Obama", 170);
+//        System.out.println(p2.name);
+        Person p2 = new Person(72, 70);
+        System.out.println(p2.age);
 
-        System.out.println(p2.name);
-
+        Person p3= p2.Call_Private();
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -14,11 +14,22 @@ public class Person {
         weight = 140;
     }
 
-    Person(double height, int age, String color, String name, double weight){
+    protected Person(double height, int age) {
+        this.height = height;
+        this.age = age;
+
+    }
+      private Person(double height, int age, String color, String name, double weight){
         this.height = height;
         this.age = age;
         this.color = color;
         this.name = name;
         this.weight = weight;
+    }
+    public Person Call_Private(){
+        Person p3= new Person(72, 70, "black", "Barack Obama", 170);
+        return p3;
+
+
     }
 }
