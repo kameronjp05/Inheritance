53679b0f4396413fcd73ba4340bfa24ede8e77a6
Kameron Peart <peartk1027@students.bowiestate.edu>
2026-09-17 14:45:05 -0400

    5th push -Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
+//https://github.com/kameronjp05/Inheritance.git
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
